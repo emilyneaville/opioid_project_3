e8d8a61c71ac05941f3baa24bd0ac65f4a3bc69d
Candida <candi.miranda@hotmail.com>
2023-04-08 21:42:57 -0500

    update on app.py

--- app.py
+++ app.py
@@ -5,18 +5,19 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+
 from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///titanic.sqlite")
+engine = create_engine("sqlite:///Resources/opioid_database.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
-Base.prepare(autoload_with=engine)
+Base.prepare(engine, reflect=True)
 
 # Save reference to the table
 opioid = Base.classes.opioid_ems_calls
@@ -25,7 +26,6 @@ opioid = Base.classes.opioid_ems_calls
 # Flask Setup
 #################################################
 app = Flask(__name__)
-
 
 #################################################
 # Flask Routes
@@ -41,7 +41,6 @@ def welcome():
         f"/api/v1.0/patient_veteran<br/>"
         f"/api/v1.0/patient_homeless"      
     )
-
 
 @app.route("/api/v1.0/patient_gender")
 def patient_gender():
@@ -75,44 +74,60 @@ def patient_student():
 
     return jsonify(all_students)
 
-@app.route("/api/v1.0/patient_gender")
-def patient_gender():
+@app.route("/api/v1.0/patient_veteran")
+def veteran():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
     """Return a list of all passenger names"""
     # Query all passengers
-    results = session.query(opioid.Patient_Gender).all()
+    results = session.query(opioid.Patient_Veteran).all()
 
     session.close()
 
     # Convert list of tuples into normal list
-    all_genders = list(np.ravel(results))
+    all_veterans= list(np.ravel(results))
 
-    return jsonify(all_genders)
+    return jsonify(all_veterans)
 
-@app.route("/api/v1.0/passengers")
-def passengers():
+@app.route("/api/v1.0/patient_homeless")
+def homeless():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    """Return a list of all passenger names"""
     # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+    results = session.query(opioid.Patient_Homeless).all()
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
+    # Convert list of tuples into normal list
+    all_homeless = list(np.ravel(results))
 
-    return jsonify(all_passengers)
+    return jsonify(all_homeless)
 
+
+# @app.route("/api/v1.0/passengers")
+# def passengers():
+#     # Create our session (link) from Python to the DB
+#     session = Session(engine)
+
+#     """Return a list of passenger data including the name, age, and sex of each passenger"""
+#     # Query all passengers
+#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+#     session.close()
+
+#     # Create a dictionary from the row data and append to a list of all_passengers
+#     all_passengers = []
+#     for name, age, sex in results:
+#         passenger_dict = {}
+#         passenger_dict["name"] = name
+#         passenger_dict["age"] = age
+#         passenger_dict["sex"] = sex
+#         all_passengers.append(passenger_dict)
+
+#     return jsonify(all_passengers)
 
 if __name__ == '__main__':
     app.run(debug=True)
